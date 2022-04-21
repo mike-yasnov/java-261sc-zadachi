92a38979efaf78883910b7fe112a753d705c3bcd
Yasnov Mikhail <mike_yasnov@mail.ru>
2022-04-21 11:48:10 +0300

    Update z112339.java

--- sc/src/z112339.java
+++ sc/src/z112339.java
@@ -4,7 +4,7 @@ public class z112339 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+        String s = sc.nextLine();
         String[] words = s.split(" ");
         System.out.print(words[0]);
 
