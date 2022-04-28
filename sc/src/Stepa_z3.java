ca12459c83ab2b0084b27876775ff0cb749de8a6
Yasnov Mikhail <mike_yasnov@mail.ru>
2022-04-28 11:46:30 +0300

    Create Stepa_z3.java

---
+++ sc/src/Stepa_z3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Stepa_z3 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        boolean flag = false;
+
+
+
+
+
+        }
+    }
+}
+
